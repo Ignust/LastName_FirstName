0075199930e41a0a4a9a58a3e76a915199e80926
Ignust <andrey.las90@gmail.com>
2020-01-15 17:14:33 +0200

    renamed Timer attributes

--- projectPacman/Timer/Timer.hpp
+++ projectPacman/Timer/Timer.hpp
@@ -24,17 +24,17 @@ public:
     void start(const Interval& interval, const Timeout& timeout)
     {
         stop();
-        m_running = true;
+        m_running_ = true;
 
-        m_thread = std::thread([=]()
+        m_thread_ = std::thread([=]()
         {
-            while (m_running)
+            while (m_running_)
             {
-                std::unique_lock<std::mutex> lock(m_mutex);
+                std::unique_lock<std::mutex> lock(m_mutex_);
                 const auto end = std::chrono::steady_clock::now() + interval;
-                if (cv.wait_for(lock, interval, [=]{return (!m_running || (std::chrono::steady_clock::now() >= end));}))
+                if (cv_.wait_for(lock, interval, [=]{return (!m_running_ || (std::chrono::steady_clock::now() >= end));}))
                 {   // timeout expired and timer was not stopped
-                    if (m_running && timeout) timeout();
+                    if (m_running_ && timeout) timeout();
                 }
             }
         });
@@ -43,23 +43,23 @@ public:
     void startOneShot(const Interval& interval, const Timeout& timeout)
     {
         stop();
-        m_running = true;
+        m_running_ = true;
 
-        m_thread = std::thread([=]()
+        m_thread_ = std::thread([=]()
         {
-            std::unique_lock<std::mutex> lock(m_mutex);
+            std::unique_lock<std::mutex> lock(m_mutex_);
             std::chrono::time_point<std::chrono::steady_clock> now = std::chrono::steady_clock::now();
             const auto end = now + interval;
-            while (m_running && now < end)
+            while (m_running_ && now < end)
             {
-                if (cv.wait_until(lock, end, [=, &now]
+                if (cv_.wait_until(lock, end, [=, &now]
                 {
                     now = std::chrono::steady_clock::now();
-                    return (!m_running || (now >= end));
+                    return (!m_running_ || (now >= end));
                 }))
                 {
                     // timeout expired and timer was not stopped
-                    if (m_running && timeout)
+                    if (m_running_ && timeout)
                     {
                         timeout();
                     }
@@ -70,19 +70,19 @@ public:
 
     void stop()
     {
-        m_running = false;
-        cv.notify_one();
-        if (m_thread.joinable())
+        m_running_ = false;
+        cv_.notify_one();
+        if (m_thread_.joinable())
         {
-            m_thread.join();
+            m_thread_.join();
         }
     }
 
 private:
-    std::thread m_thread;
-    std::atomic_bool m_running { false };
-    mutable std::mutex m_mutex;
-    std::condition_variable cv;
+    std::thread m_thread_;
+    std::atomic_bool m_running_ { false };
+    mutable std::mutex m_mutex_;
+    std::condition_variable cv_;
 };
 
 }
